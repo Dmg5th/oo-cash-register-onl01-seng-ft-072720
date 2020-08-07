45ad89ffeddcc38697a82d04290c7574f66cbe62
Daniel Gould <dmgould310@gmail.com>
2020-08-07 20:03:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :reader 
+  attr_accessor :reader
 
   def initialize(discount = 20)
     @total = 0 
+    @discount = discount 
   end 
   
   
