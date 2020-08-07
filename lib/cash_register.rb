cfc1eb1e69e7a59adfbfd048e6b018b3aef1f9e2
Daniel Gould <dmgould310@gmail.com>
2020-08-07 20:07:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,9 +6,12 @@ class CashRegister
   def initialize(discount = 20)
     @total = 0 
     @discount = discount 
+    @items = []
   end 
   
-  def add_item 
+  def add_item(title, price, quantity = 1)
+    @total = price * quantity 
+    
     
   end 
   
