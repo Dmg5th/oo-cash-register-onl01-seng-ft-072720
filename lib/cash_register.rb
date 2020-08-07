31fe7c5a0be6c6d39732c93036ce04d621d78b09
Daniel Gould <dmgould310@gmail.com>
2020-08-07 20:09:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,17 +10,28 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity = 1)
-    @total = price * quantity 
-    new_total = @total 
-    
-    
+    @total += price * quantity 
   end 
   
  
   
   
   
-end 
+end
+
+require 'pry'
+
+class CashRegister
+  attr_accessor :total, :discount, :price 
+  attr_reader :items 
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end 
+  
+
 
 
 
